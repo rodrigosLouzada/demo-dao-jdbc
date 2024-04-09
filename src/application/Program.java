b004c90b39352cc43f284784f9c244d4a72a1b5b
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-04-09 10:58:34 -0300

    findBydepartmentId complete, filter using if, hashmap, list for

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -15,12 +16,20 @@ public class Program {
 		//Seller seller = new Seller(21, "DinhoOGordinho",
 				//"DinhoGordinho@gmail.com",new Date(), 3000.0, obj);
 		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
+		SellerDao sellerDao = DaoFactory.createSellerDao(); //seller from daofactory for tests
 		
 		System.out.println("=== test, findById ===");
 		
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
+		System.out.println("\n=== test2, SellerByDepartement ===");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
