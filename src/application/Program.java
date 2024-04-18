4376963c148e38d8665e3c04dbb991136c8afaf4
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-04-18 11:18:33 -0300

    delete implementation, filter

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -15,6 +16,7 @@ public class Program {
 		//Department obj = new Department(1, "Books");
 		//Seller seller = new Seller(21, "DinhoOGordinho",
 				//"DinhoGordinho@gmail.com",new Date(), 3000.0, obj);
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); //seller from daofactory for tests
 		
@@ -54,7 +56,18 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("update complete!");
 		
+		////
+		System.out.println("\n=== test6, SellerDelete ===");
+		//sellerDao.deleteById(seller.getId());
+		System.out.println("enter id for delete: ");
 		
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		
+		System.out.println("delete complete");
+		
+		sc.close();
+	
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -100,9 +100,34 @@ public class SellerDaoJDBC implements SellerDao {
 		
 	}
 
+	
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"DELETE FROM seller "
+					+ "WHERE Id = ?");
+			
+			st.setInt(1, id);
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected == 0) {
+				throw new DbException("error, no rows affected, try an id that exists");
+			}
+			
+			
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+			
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+		
+		
+		
 		
 	}
 
