8402d0337610ac6b1a0190f6d0d9860bf342e344
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2024-04-22 11:15:04 -0300

    incomplete implementation: department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,9 +1,34 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.dao.impl.DepartmentDaoJDBC;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
-
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		Department dep = new Department(4, "DinhoGordo");
+		
+		///test
+		
+		departmentDao.update(dep);
+		System.out.println("update complete");
+		
+		departmentDao.deleteById(dep.getId());;
+		System.out.println("delete completed, your id is: " + dep.getName());
+		
+		departmentDao.insert(dep);
+		System.out.println("insert complete, your new department'name: "  );
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,11 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -14,21 +17,83 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 		this.conn = conn;
 	}
 
+	
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		
+		try { st = conn.prepareStatement("INSERT INTO department "
+				+ "(Id, Name)"
+				+ "VALUES "
+				+ "?, ?",
+				st.RETURN_GENERATED_KEYS);
+		
+		st.setInt(1, obj.getId());
+		st.setString(2, obj.getName());
+		
+		int rowsaff = st.executeUpdate();
+		
+		if( rowsaff > 0) {
+			
+		}
+		
+		
+		
+		
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
 
 	}
+	
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st =  null;
+		
+		try { st = conn.prepareStatement("UPDATE Department "
+				+ "SET Id = ?, Name = ?");
+		
+			st.setInt(1, obj.getId());
+			st.setString(2, obj.getName());
+			
+			int rowsaff = st.executeUpdate();
+			
+			if(rowsaff == 0) {
+				throw new DbException("try again, no updates!");
+			}
+			
+		}catch (Exception e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
+	
+	
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement("DELETE FROM Department "
+				+ "WHERE Id = ?");
+		
+			st.setInt(1, id);
+			int rowsff = st.executeUpdate();
+		
+			if(rowsff == 0) {
+				throw new DbException("error, no rows affected, try an id that exists");
+			
+		}
+			
+				
+		}catch (Exception e) {
+			throw new DbException(e.getMessage());
+		}
 
 	}
 
